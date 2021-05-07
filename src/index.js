bb6201aff68ed732a489ad3c18894a586ac23582
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-07 22:33:23 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -54,15 +54,34 @@
  * テンプレート文字列
  */
 
-const name = "てけ";
-const age = "32";
+// const name = "てけ";
+// const age = "32";
 
 // 「私の名前はてけです。年齢は32歳です
 
 // 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
 
 //テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です`;
-console.log(message2);
+// const message2 = `私の名前は${name}です。年齢は${age}歳です`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+
+// 従来の関数
+//  function func1(str) {
+//    return str;
+//  }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
