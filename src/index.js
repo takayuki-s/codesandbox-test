206d99f694d661476903ff792c5a440d8b9961ac
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-08 13:08:38 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -75,24 +75,48 @@
 //  function func1(str) {
 //    return str;
 //  }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
 // アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
 //アロー関数 {}中身がreturnのみだと{}とreturn省略可能
-const func3 = (str) => str;
-console.log(func3("func3です"));
+// const func3 = (str) => str;
+// console.log(func3("func3です"));
 
 // アロー関数 引数2つ
-const func4 = (num1, num2) => {
-  return num1 + num2;
-};
+// const func4 = (num1, num2) => {
+//   return num1 + num2;
+// };
 
-console.log(func4(10, 20));
+// console.log(func4(10, 20));
+
+/**
+ * 分割代入
+ */
+
+// const myProfile = {
+//   name: "てけ",
+//   age: 32
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `私の名前は${name}です。年は${age}歳です。`;
+// console.log(message2);
+
+const myProfile = ["てけ", 32];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `私の名前は${name}です。歳は${age}です`;
+console.log(message4);
