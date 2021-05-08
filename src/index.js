54b9d7a01af02b4c366b4c04e774cb6f493a85ae
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-08 13:19:32 +0900

    スプレッド構文基礎

--- src/index.js
+++ src/index.js
@@ -125,5 +125,19 @@
  * デフォルト、引数
  */
 
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
-sayHello("てけ");
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+// sayHello("てけ");
+
+/**
+ * スプレッド構文
+ */
+
+// 配列の展開
+
+const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
