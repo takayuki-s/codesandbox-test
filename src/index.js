c20631d64c6a8bf342debc3e7f6a0e3d7b967ba2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-09 22:04:50 +0900

    map簡易記述

--- src/index.js
+++ src/index.js
@@ -166,11 +166,13 @@
  */
 
 const nameArr = ["田中", "山田", "鈴木"];
-for (let index = 0; index < nameArr.length; index++) {
-  console.log(nameArr[index]);
-}
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
 
-const nameArr2 = nameArr.map((name) => {
-  return name;
-});
-console.log(nameArr2);
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+nameArr.map((name) => console.log(name));
